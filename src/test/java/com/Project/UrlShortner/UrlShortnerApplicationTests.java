bf5413aecca23f2232f95bf1618eefff47ee22a7
vijayan-0908 <ujjvalm_chouhan@yahoo.com>
2026-02-28 17:38:31 +0530

    Initial commit

---
+++ src/test/java/com/Project/UrlShortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.UrlShortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
